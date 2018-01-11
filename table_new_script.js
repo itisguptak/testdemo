60ddf86104909a7ecfb2d4e1e5c8e8e779516271
Karan Gupta <itis.guptak@gmail.com>
2018-01-11 14:18:28 +0530

    date : 1/11/2018

--- table_new_script.js
+++ table_new_script.js
@@ -2,7 +2,11 @@
 
 function delete_row(no)
 {
- document.getElementById("row"+no+"").outerHTML="";
+ /*document.getElementById("row"+no+"").outerHTML="";*/
+	 
+	 document.getElementById("data_table").deleteRow("row"+no+"");
+	  
+
 }
 
 function add_row()
@@ -14,18 +18,17 @@ function add_row()
 	 var table=document.getElementById("data_table");
 	 var table_len=(table.rows.length);
 	 var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'>" +
-	 		"<td id='ip_col"+table_len+"'>" +
-	 				"<label id='IP Address'>IP Address : </label> " +
-	 				"<input type='text' disabled='disabled' id='new_ip"+table_len+"' value='"+new_ip+"' ></td>" +
-	 		"<td id='port_col"+table_len+"'>" +
-	 				"<label id='Port'>Port : </label> " +
-	 				"<input type='text' id='new_port"+table_len+"' disabled='disabled' value='"+new_port+"' ></td>" +
-	 		"<td id='label_col"+table_len+"'>" +
-	 				"<label id='Label'>Label : </label> " +
-	 				"<input type='text' id='new_label"+table_len+"' disabled='disabled' value='"+new_label+"' ></td>" +
-	 		"<td> <input type='button' value='-' onclick='delete_row("+table_len+")'></td>"+
-	 		"</tr>";
-
+		"<td id='ip_col"+table_len+"'>" +
+				"<label id='IP Address'>IP Address : </label> " +
+				"<input type='text' disabled='disabled' id='new_ip"+table_len+"' name='ip' value='"+new_ip+"' ></td>" +
+		"<td id='port_col"+table_len+"'>" +
+				"<label id='Port'>Port : </label> " +
+				"<input type='text' id='new_port"+table_len+"' name='port' disabled='disabled' value='"+new_port+"' ></td>" +
+		"<td id='label_col"+table_len+"'>" +
+				"<label id='Label'>Label : </label> " +
+				"<input type='text' id='new_label"+table_len+"' name='label' disabled='disabled' value='"+new_label+"' ></td>" +
+		"<td> <input type='button' value='-' onclick='delete_row("+table_len+")'></td>"+
+		"</tr>";
 	 document.getElementById("new_ip0").value="";
 	 document.getElementById("new_port0").value="";
 	 document.getElementById("new_label0").value="";
